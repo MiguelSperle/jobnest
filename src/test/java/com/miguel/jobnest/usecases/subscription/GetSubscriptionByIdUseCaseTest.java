9c578917d9cb41e0fb8e84de12f1ba70e0bab36b
MiguelSperle <miguelsperledeveloper@gmail.com>
2025-12-12 18:48:10 -0300

    feat: implement unit tests of DefaultGetSubscriptionByIdUseCase

---
+++ src/test/java/com/miguel/jobnest/usecases/subscription/GetSubscriptionByIdUseCaseTest.java
@@ -0,0 +1,80 @@
+package com.miguel.jobnest.usecases.subscription;
+
+import com.miguel.jobnest.application.abstractions.repositories.SubscriptionRepository;
+import com.miguel.jobnest.application.usecases.subscription.DefaultGetSubscriptionByIdUseCase;
+import com.miguel.jobnest.application.usecases.subscription.inputs.GetSubscriptionByIdUseCaseInput;
+import com.miguel.jobnest.application.usecases.subscription.outputs.GetSubscriptionByIdUseCaseOutput;
+import com.miguel.jobnest.domain.entities.JobVacancy;
+import com.miguel.jobnest.domain.entities.Subscription;
+import com.miguel.jobnest.domain.entities.User;
+import com.miguel.jobnest.domain.enums.AuthorizationRole;
+import com.miguel.jobnest.domain.enums.UserStatus;
+import com.miguel.jobnest.domain.exceptions.NotFoundException;
+import com.miguel.jobnest.domain.utils.IdentifierUtils;
+import com.miguel.jobnest.utils.JobVacancyBuilderTest;
+import com.miguel.jobnest.utils.SubscriptionBuilderTest;
+import com.miguel.jobnest.utils.UserBuilderTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class GetSubscriptionByIdUseCaseTest {
+    @InjectMocks
+    private DefaultGetSubscriptionByIdUseCase useCase;
+
+    @Mock
+    private SubscriptionRepository subscriptionRepository;
+
+    @Test
+    void shouldGetSubscriptionById() {
+        final User userCandidate = UserBuilderTest.build(UserStatus.VERIFIED, AuthorizationRole.CANDIDATE);
+        final User userRecruiter = UserBuilderTest.build(UserStatus.VERIFIED, AuthorizationRole.RECRUITER);
+        final JobVacancy jobVacancy = JobVacancyBuilderTest.build(userRecruiter.getId());
+        final Subscription subscription = SubscriptionBuilderTest.build(userCandidate.getId(), jobVacancy.getId());
+
+        final GetSubscriptionByIdUseCaseInput input = GetSubscriptionByIdUseCaseInput.with(
+                subscription.getId()
+        );
+
+        Mockito.when(this.subscriptionRepository.findById(Mockito.any())).thenReturn(Optional.of(subscription));
+
+        final GetSubscriptionByIdUseCaseOutput output = this.useCase.execute(input);
+
+        Assertions.assertNotNull(output);
+        Assertions.assertNotNull(output.subscription());
+        Assertions.assertEquals(subscription.getId(), output.subscription().getId());
+        Assertions.assertEquals(subscription.getUserId(), output.subscription().getUserId());
+        Assertions.assertEquals(subscription.getJobVacancyId(), output.subscription().getJobVacancyId());
+        Assertions.assertEquals(subscription.getResumeUrl(), output.subscription().getResumeUrl());
+        Assertions.assertEquals(subscription.getIsCanceled(), output.subscription().getIsCanceled());
+        Assertions.assertEquals(subscription.getCreatedAt(), output.subscription().getCreatedAt());
+
+        Mockito.verify(this.subscriptionRepository, Mockito.times(1)).findById(Mockito.any());
+    }
+
+    @Test
+    void shouldThrowNotFoundException_whenSubscriptionDoesNotExist() {
+        final GetSubscriptionByIdUseCaseInput input = GetSubscriptionByIdUseCaseInput.with(
+                IdentifierUtils.generateUUID()
+        );
+
+        Mockito.when(this.subscriptionRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+        final var ex = Assertions.assertThrows(NotFoundException.class, () ->
+                this.useCase.execute(input)
+        );
+
+        final String expectedErrorMessage = "Subscription not found";
+
+        Assertions.assertEquals(expectedErrorMessage, ex.getMessage());
+
+        Mockito.verify(this.subscriptionRepository, Mockito.times(1)).findById(Mockito.any());
+    }
+}
